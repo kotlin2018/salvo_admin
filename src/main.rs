2dbe1be911fe10f5034ebfa8715eba1e8c37bf2b
li ou <3212728662@qq.com>
2022-07-11 16:00:02 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ extern crate rbatis;
 use salvo::prelude::*;
 use crate::controller::init_router;
 use crate::entity::{ApplicationConfig, init_rbatis};
-
+#[allow(dead_code,unused)]
 mod entity;
+#[allow(dead_code,unused)]
 mod controller;
+#[allow(dead_code,unused)]
 mod service;
+#[allow(dead_code,unused)]
 mod dto;
 
 #[tokio::main]
 async fn main() {
-    let rb = init_rbatis().await;
     let server_url = ApplicationConfig::default().server_url;
     tracing_subscriber::fmt::init();
     tracing::info!("Listening on {:?}",&server_url);//这里传 server_url 的引用

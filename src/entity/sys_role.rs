bd1ebf499fb9dee95389f8e1919282593700605d
li ou <3212728662@qq.com>
2023-02-18 13:15:00 +0800

    update

---
+++ src/entity/sys_role.rs
@@ -0,0 +1,45 @@
+use rbatis::crud;
+use rbatis::rbdc::datetime::FastDateTime;
+
+#[derive(Debug)]
+pub struct SysRole {
+    // 角色编码
+    pub role_id: Option<i32>,
+    // 角色名称
+    pub role_name: Option<String>,
+
+    pub status: Option<String>,
+    // 角色代码
+    pub role_key: Option<String>,
+    // 角色排序
+    pub role_sort: Option<i8>,
+    //
+    pub flag: Option<String>,
+    // 备注
+    pub remark: Option<String>,
+    pub admin: Option<bool>,
+    pub data_scope: Option<String>,
+    // 创建者
+    pub create_by: Option<i32>,
+    // 更新者
+    pub update_by: Option<i32>,
+    // 创建时间
+    pub created_at: Option<FastDateTime>,
+    // 最后更新时间
+    pub updated_at: Option<FastDateTime>,
+    // 删除时间
+    pub deleted_at: Option<FastDateTime>,
+}
+crud!(SysRole{});
+
+pub struct SysRoleDept {
+    pub role_id: Option<i32>,
+    pub dept_id: Option<i32>,
+}
+crud!(SysRoleDept{});
+
+pub struct SysRoleMenu {
+    pub role_id: Option<i32>,
+    pub menu_id: Option<i32>,
+}
+crud!(SysRoleMenu{});
---
+++ src/entity/sys_user.rs
@@ -0,0 +1,45 @@
+use rbatis::crud;
+use rbatis::rbdc::datetime::FastDateTime;
+
+#[derive(Debug)]
+pub struct SysUser {
+    // 编码
+    pub user_id: Option<i32>,
+    // 用户名
+    pub username: Option<String>,
+    // 密码
+    pub password: Option<String>,
+    // 昵称
+    pub nickname: Option<String>,
+    // 手机号
+    pub phone: Option<String>,
+    // 角色ID
+    pub role_id: Option<i32>,
+    // 加盐
+    pub salt: Option<String>,
+    // 头像
+    pub avatar: Option<String>,
+    // 性别
+    pub sex: Option<String>,
+    // 邮箱
+    pub email: Option<String>,
+    // 部门
+    pub dept_id: Option<i32>,
+    // 岗位
+    pub post_id: Option<i32>,
+    // 备注
+    pub remark: Option<String>,
+    // 状态
+    pub status: Option<String>,
+    // 创建者
+    pub create_by: Option<i32>,
+    // 更新者
+    pub update_by: Option<i32>,
+    // 创建时间
+    pub created_at: Option<FastDateTime>,
+    // 最后更新时间
+    pub updated_at: Option<FastDateTime>,
+    // 删除时间
+    pub deleted_at: Option<FastDateTime>,
+}
+crud!(SysUser{});

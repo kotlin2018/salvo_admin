0893a2cea0a0b2c1d8be9ca8c07010594935aa95
li ou <3212728662@qq.com>
2023-02-19 14:19:18 +0800

    update

--- src/mapper/mod.rs
+++ src/mapper/mod.rs
@@ -1,4 +1,5 @@
 use rbatis::{executor::Executor, impled,Result};
+use crate::entity::sys_dept::SysDept;
 use crate::entity::sys_role::SysRole;
 use crate::entity::sys_user::SysUser;
 
@@ -6,11 +7,14 @@ use crate::entity::sys_user::SysUser;
 // pub async fn permission(rb: &mut dyn Executor,data_scope: i32,table_name: &str,dept_path: Option<&str>,user_id: Option<i32>,role_id: Option<i32>, dept_id: Option<i32>){todo!()}
 
 #[html_sql("src/mapper/salvo_admin.html")]
-pub async fn get_user(rb: &mut dyn Executor,username: String,password: String) -> rbatis::Result<SysUser>{impled!()}
+pub async fn get_user(rb: &mut dyn Executor,username: String,password: String,page_no:i32,page_size: i32) -> rbatis::Result<Vec<SysUser>>{impled!()}
 
 
 #[html_sql("src/mapper/salvo_admin.html")]
 pub async fn get_role(rb: &mut dyn Executor,role_id: i32) -> rbatis::Result<SysRole>{impled!()}
+
+#[html_sql("src/mapper/salvo_admin.html")]
+pub async fn get_dept(rb: &mut dyn Executor,dept_id: i32) -> Result<SysDept>{impled!()}
 
 #[cfg(test)]
 mod tests {
@@ -21,7 +25,8 @@ mod tests {
     #[tokio::test]
     async fn test_html_file_sql(){
         let mut rb = &CONTEXT.db;
-        let user = get_user(&mut rb,"admin".to_string(),"123456".to_string()).await.unwrap();
+        let password = "$2a$10$/Glr4g9Svr6O0kvjsRJCXu3f0W8/dsP3XZyVNi1019ratWpSPMyw.".to_string();
+        let user = get_user(&mut rb,"admin".to_string(),password,1,0).await.unwrap();
         println!("{user:?}")
     }
 }

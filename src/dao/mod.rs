d519e77d7e6d49c72004a9851a87ea2978faf3a2
li ou <3212728662@qq.com>
2023-02-19 10:24:12 +0800

    update

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -18,54 +18,6 @@ mod tests {
     use super::*;
     use crate::config::CONTEXT;
 
-    #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
-    pub struct BizActivity {
-        pub id: Option<String>,
-        pub name: Option<String>,
-        pub pc_link: Option<String>,
-        pub h5_link: Option<String>,
-        pub pc_banner_img: Option<String>,
-        pub h5_banner_img: Option<String>,
-        pub sort: Option<String>,
-        pub status: Option<i32>,
-        pub remark: Option<String>,
-        pub create_time: Option<FastDateTime>,
-        pub version: Option<i64>,
-        pub delete_flag: Option<i32>,
-    }
-
-
-    #[html_sql("mapper/example.html")]
-    async fn select_by_condition(
-        rb: &mut dyn Executor,
-        name: &str,
-        dt: &FastDateTime,
-    ) -> rbatis::Result<Vec<BizActivity>> {
-        impled!()
-    }
-
-    #[tokio::test]
-    async fn test_example_html_sql(){
-        let mut rb = &CONTEXT.db;
-        let a = select_by_condition(&mut rb.clone(), "test", &FastDateTime::now().set_micro(0))
-            .await
-            .unwrap();
-        println!("{:?}", a);
-    }
-
-    #[html_sql("mapper/example.html")]
-    async fn get_obj(rb: &mut dyn Executor) -> rbatis::Result<Vec<BizActivity>>{impled!()}
-
-    #[tokio::test]
-    async fn test_example_html_sql2(){
-        let mut rb = &CONTEXT.db;
-        let a = get_obj(&mut rb)
-            .await
-            .unwrap();
-        println!("{:?}", a);
-    }
-
-
     #[tokio::test]
     async fn test_html_file_sql(){
         let mut rb = &CONTEXT.db;
